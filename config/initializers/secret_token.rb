35e5384c43f8f97cb1761dffc3ee37c927b5a33a
firstblood1985 <limin.hust@foxmail.com>
2013-02-28 22:13:56 +0800

    Yet Another Reservation System

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YARS::Application.config.secret_token = '1fa7366f9058aff2472853875d03d62680830eba6cab7a4cf48d0b566b2f621e8ad757d642f442b74baca5ea9e5169103e67196e9da849fd0afe1b73d6e89e5a'
